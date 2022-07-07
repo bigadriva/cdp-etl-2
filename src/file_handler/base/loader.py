56be574f60ca577b196052cf35338bd9deffc4e0
Matheus Bigarelli <biga@driva.com.br>
2022-07-07 16:22:00 -0300

    add: load individual tables to loader interface

--- src/file_handler/base/loader.py
+++ src/file_handler/base/loader.py
@@ -1,9 +1,24 @@
 from abc import ABC, abstractmethod
 
 from models.mapping.database import DatabaseMapping
+from models.mapping.products import ProductsMapping
+from models.mapping.sales import SalesMapping
+from models.mapping.salespeople import SalespeopleMapping
 
 
 class Loader(ABC):
     @abstractmethod
     def load(self, company_name: str, database_mapping: DatabaseMapping):
+        pass
+
+    @abstractmethod
+    def load_sales(self, sales_mapping: SalesMapping):
+        pass
+
+    @abstractmethod
+    def load_products(self, products_mapping: ProductsMapping):
+        pass
+
+    @abstractmethod
+    def load_salespeople(self, salespeople_mapping: SalespeopleMapping):
         pass
